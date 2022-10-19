cf759c113683ef443bb7ca68607387d5c3a06564
Guilherme de Almeida <guilherme.10k@hotmail.com>
2022-10-19 19:11:41 -0300

    Update js.js

--- JS/js.js
+++ JS/js.js
@@ -1,6 +1,6 @@
 //window.alert('Desenvolvimento de um jogo com JavaScript')
 //window.prompt('Qual o seu nome?')
-//var nome = window.prompt('Seja bem vindo! Qual o seu nome?')
+var nome = window.prompt('Seja bem vindo! Qual o seu nome?')
 var nomediv = window.document.getElementById('nome').innerText = `Olá, ${nome}, espero que minha calculadora possa te ajudar!`
 
 
